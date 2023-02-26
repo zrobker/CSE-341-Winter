9d6db3433b297422e59ea10c4c5df8b4c6e7be99
Zach Robker <zrobker@byui.edu>
2023-02-26 00:57:15 -0500

    confusion

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,8 +2,7 @@ const routes = require('express').Router();
 
 const myController = require('../controllers');
 
-routes.get('/', myController.awesomeFunction);
-
-routes.use('/contacts', require('/contacts'));
+routes.get('/all', myController. getAll);
+routes.get('/:id', myController. getSingle);
 
 module.exports = routes;
